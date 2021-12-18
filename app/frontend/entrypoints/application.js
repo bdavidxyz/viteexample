ab0097390d31a07c28710579616be9472060e210
David Boureau <bdavidxyz@gmail.com>
2021-12-18 19:48:52 +0100

    full reload woooorks !

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -3,7 +3,7 @@
 //
 //    <%= vite_client_tag %>
 //    <%= vite_javascript_tag 'application' %>
-console.log('Vite ⚡️ Rails')
+console.log('Vite ⚡️ Rails 42')
 
 // If using a TypeScript entrypoint file:
 //     <%= vite_typescript_tag 'application' %>
